d2be1b9b0f7eebf7bc26386413bde6eeb297ed14
Mohamed Chiboub <mohamedchiboub97@gmail.com>
2020-04-06 17:12:17 +0100

    Update Demo.m

--- Demo/Demo.m
+++ Demo/Demo.m
@@ -9,7 +9,7 @@ out_file = 'out_hello_world';
 sig = text2morse(in_txt, fs, dot_duration, out_file, true);
 
 % Représentation temporelle du signal
-axe_t = 0:1/fs:size(sig, 2)/fs;
+axe_t = 0+1/fs:1/fs:size(sig, 2)/fs;
 plot(axe_t, sig);
 title('Audio Généré');
 xlabel('T (secondes)');
